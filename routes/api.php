b36a32987a80ec0ee9ebc9e857e7ab938954820a
sashanevirkovets <sashanevirkovets@gmail.com>
2018-10-16 12:20:32 +0300

    update env

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,13 @@ Route::post('/signup', 'AuthController@signup');
 
 Route::group(['middleware' => ['cors']], function () {
     Route::post('/login', 'AuthController@login');
+    Route::post('/reset/password/email', 'AuthController@resetPasswordEmail');
 });
 
 
 Route::group(['prefix' => 'auth', 'middleware' => 'jwt.auth'], function () {
     Route::get('user', 'AuthController@user');
     Route::post('logout', 'AuthController@logout');
-    Route::post('/reset/password/email', 'AuthController@resetPasswordEmail');
 });
 
 
